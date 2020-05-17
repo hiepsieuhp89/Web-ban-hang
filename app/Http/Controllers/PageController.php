d3410df2d1eb281040dc53b8828b186ff990b67f
Tùng Xuân <tungcan2000@gmail.com>
2020-05-17 17:15:58 +0700

    bt

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -232,27 +232,9 @@ class PageController extends Controller
         }
     }
     public function getSearch(Request $req){
-        $key_name = strtolower($req->key);
-        $utf8 = array(
-
-            'a'=>'á|à|ả|ã|ạ|ă|ắ|ặ|ằ|ẳ|ẵ|â|ấ|ầ|ẩ|ẫ|ậ|Á|À|Ả|Ã|Ạ|Ă|Ắ|Ặ|Ằ|Ẳ|Ẵ|Â|Ấ|Ầ|Ẩ|Ẫ|Ậ',
-
-            'd'=>'đ|Đ',
-
-            'e'=>'é|è|ẻ|ẽ|ẹ|ê|ế|ề|ể|ễ|ệ|É|È|Ẻ|Ẽ|Ẹ|Ê|Ế|Ề|Ể|Ễ|Ệ',
-
-            'i'=>'í|ì|ỉ|ĩ|ị|Í|Ì|Ỉ|Ĩ|Ị',
-
-            'o'=>'ó|ò|ỏ|õ|ọ|ô|ố|ồ|ổ|ỗ|ộ|ơ|ớ|ờ|ở|ỡ|ợ|Ó|Ò|Ỏ|Õ|Ọ|Ô|Ố|Ồ|Ổ|Ỗ|Ộ|Ơ|Ớ|Ờ|Ở|Ỡ|Ợ',
-
-            'u'=>'ú|ù|ủ|ũ|ụ|ư|ứ|ừ|ử|ữ|ự|Ú|Ù|Ủ|Ũ|Ụ|Ư|Ứ|Ừ|Ử|Ữ|Ự',
-
-            'y'=>'ý|ỳ|ỷ|ỹ|ỵ|Ý|Ỳ|Ỷ|Ỹ|Ỵ',
-        );
-            foreach($utf8 as $ascii=>$uni) $key_name = preg_replace("/($uni)/i",$ascii,$key_name);
         $namep = $req->key;
         $slide = Slide::all();
-        $product = Product::WHERE('name2','LIKE','%' .$key_name. '%')->paginate(16);
+        $product = Product::WHERE('name2','ILIKE','%' .$namep. '%')->paginate(16);
         return view('page.Search',compact('slide','product','namep'));
     }
     public function check(Request $request)
